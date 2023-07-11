c7217964c4a3194d6fd9ae35a2b9054d50c915ef
lyu-chunkwo <lyu.chun.kwo@gmail.com>
2023-07-11 10:27:41 +0600

    finish project

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,51 @@
 $(function () {
 
+  $('.cost__tabs-items').not('.active').hide();
+  $('.cost__controls-item').on('click', function (e) {
+    e.preventDefault();
+
+    var tabItem = $(this).closest('.cost__controls-item'),
+      tabContentItem = $('.cost__tabs-items'),
+      tabItemPosition = tabItem.data('services');
+
+    tabContentItem.filter('.cost__tabs-items-' + tabItemPosition)
+      .fadeIn()
+      .siblings()
+      .hide();
+
+    $(e.currentTarget)
+      .addClass('active')
+      .siblings()
+      .removeClass('active');
+  });
+
+
+    $(".services__checkbox, .modal__checkbox").on("change", function () {
+    if ($(this).prop("checked") === true) {
+      $('.services__btn, .modal__btn').addClass('active');
+    } else {
+      $('.services__btn, .modal__btn').removeClass('active');
+    }
+  });
+
+
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("modal__close")[0];
+    btn.onclick = function () {
+      modal.style.display = "block";
+    }
+    span.onclick = function () {
+      modal.style.display = "none";
+    }
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+
+
+  
   $('.class-name').slick({
 
   });
@@ -8,7 +54,7 @@ $(function () {
   $(document).scroll(function () {
     var scrollTop = $(window).scrollTop();
     console.log(scrollTop);
-    if (scrollTop >= 450) {
+    if (scrollTop >= 550) {
       $('.scroll').addClass("active");
     }
     else {
@@ -21,4 +67,5 @@ $(function () {
     $('.menu__list, .menu__btn').toggleClass('active');
     $('body').toggleClass('lock'); // ! No scroll
   });
+
 });
